38387b725c3ec6bafdd7096fb506095c61407dd5
vishalkevin11 <vishalkevin11@gmail.com>
2021-12-17 15:39:06 +0530

    Update app_language.dart

--- lib/localization/app_language.dart
+++ lib/localization/app_language.dart
@@ -42,45 +42,10 @@ class AppLanguage extends ChangeNotifier {
               country,
             );
     }
-    print('upadted $appLocal');
     notifyListeners();
   }
 
   bool get isEnglishLocale {
-    print(appLocal.languageCode);
     return appLocal.languageCode == 'en';
   }
 }
-
-// class AppLanguage extends ChangeNotifier {
-//   Locale? _appLocale = Locale('en');
-
-//   Locale get appLocal => _appLocale ?? Locale("en_US");
-//   fetchLocale() async {
-//     var prefs = await SharedPreferences.getInstance();
-//     if (prefs.getString('language_code') == null) {
-//       _appLocale = Locale('en');
-//       return Null;
-//     }
-//     _appLocale = Locale(prefs.getString('language_code'));
-//     return Null;
-//   }
-
-
-//   void changeLanguage(Locale type) async {
-//     var prefs = await SharedPreferences.getInstance();
-//     if (_appLocale == type) {
-//       return;
-//     }
-//     if (type == Locale("ar")) {
-//       _appLocale = Locale("ar");
-//       await prefs.setString('language_code', 'ar');
-//       await prefs.setString('countryCode', '');
-//     } else {
-//       _appLocale = Locale("en_US");
-//       await prefs.setString('language_code', 'en');
-//       await prefs.setString('countryCode', 'US');
-//     }
-//     notifyListeners();
-//   }
-// }
