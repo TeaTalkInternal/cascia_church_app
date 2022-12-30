fa7ed050a464c12d44fc25877f9885333f4011ad
vishalkevin11 <vishalkevin11@gmail.com>
2022-12-30 10:54:45 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  //new
   //testWidgets('Counter increments smoke test', (WidgetTester tester) async {
   // Build our app and trigger a frame.
   // await tester.pumpWidget(const MyApp());
